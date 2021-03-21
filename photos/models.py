3b7c06ca6f7e324df845325ac95b21f60b8948e6
samfan01 <dsamfan@gmail.com>
2021-03-21 23:46:30 +0300

    created the photo model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to = 'photos/')
+    photo_location = models.CharField(max_length = 60)
+    photo_category = models.CharField(max_length = 60)
+    
